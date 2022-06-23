58ba78371c5809dcf039bd8e49cab043e21c4381
Marcin Bandos <80113567+marycin@users.noreply.github.com>
2022-06-23 13:31:01 +0200

    galeria-poprawa

--- projektio/settings.py
+++ projektio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'projektio',
         'USER': 'postgres',
-        'PASSWORD': 'Mnbvcxz+1',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
